42c2c9deb292943edde48db10bfa1499dcc78f0a
Madhuri Sonawane <madhurisonawane851@gmail.com>
2026-02-08 14:57:17 +0530

    CrudAppV3

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/users";
+const API_URL = "https://react-crud-json-server-ljgs.onrender.com/users";
 
 export const getUsers = () => axios.get(API_URL);
-export const createUser = (data) => axios.post(API_URL, data);
-export const updateUser = (id, data) => axios.put(`${API_URL}/${id}`, data);
-export const deleteUser = (id) => axios.delete(`${API_URL}/${id}`);
+
+export const createUser = (data) =>
+  axios.post(API_URL, data);
+
+export const updateUser = (id, data) =>
+  axios.put(`${API_URL}/${id}`, data);
+
+export const deleteUser = (id) =>
+  axios.delete(`${API_URL}/${id}`);
